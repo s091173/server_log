b1d2e5fc9caf2bc7f7495267c23f884f264c5288
Yuchi Su <astraiostor@gmail.com>
2021-01-10 18:10:52 +0800

    feat: add server log function

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const moment = require('moment')
+
+app.use((req, res, next) => {
+  // request method 
+  const method = req.method
+  // request url
+  const url = req.originalUrl
+  // request time
+  const timestamps = moment().format('YYYY-MM-DD HH:mm:ss')
+  // sever log 
+  console.log(`${timestamps} | ${method} from ${url}`)
+  // call the next middleware
+  next()
+})
 
 app.get('/', (req, res) => {
   res.send('列出全部 Todo')
