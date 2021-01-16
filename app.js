39863b95590ffb4225470ef73a336b12ae1f556d
Yuchi Su <astraiostor@gmail.com>
2021-01-16 15:59:00 +0800

    feat: prevent favicon request

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 3000
 const moment = require('moment')
 
+// avoid favicon requests
+const icon = '<link rel="icon" href="data:,"></link>'
+
 app.use((req, res, next) => {
   // request method 
   const method = req.method
@@ -17,19 +20,19 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-  res.send('列出全部 Todo')
+  res.send(icon + '列出全部 Todo')
 })
 
 app.get('/new', (req, res) => {
-  res.send('新增 Todo 頁面')
+  res.send(icon + '新增 Todo 頁面')
 })
 
 app.get('/:id', (req, res) => {
-  res.send('顯示一筆 Todo')
+  res.send(icon + '顯示一筆 Todo')
 })
 
 app.post('/', (req, res) => {
-  res.send('新增一筆  Todo')
+  res.send(icon + '新增一筆  Todo')
 })
 
 app.listen(port, () => {
